0fcc2223f86f375e0ddf8912ac671251ab03db66
Anna <annaocirova5@gmail.com>
2023-07-19 16:03:54 +0800

    Создание модели

--- Models/Users.cs
+++ Models/Users.cs
@@ -3,9 +3,23 @@
     public class Users
     {
         public int Id { get; set; }
+        public int IdOfOrganization { get; set; }
+        public string Firstname { get; set; }
         public string Username { get; set; }
+        public string Surname { get; set; }
         public string Email { get; set; }
+        public string Status { get; set; }
+    }
+    public class UsersDTO
+    {
+        public int Id { get; set; }
+        public int IdOfOrganization { get; set; }
+        public string Firstname { get; set; }
+        public string Username { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Status { get; set; }
+        public string Login { get; set; }
         public string Password { get; set; }
-        public bool IsBlocked { get; set; } = false;
     }
 }
